9c6ee3aab79f54ef2a632ad370a7ac3d3738b1cd
Yamac Ozdil <yamac.ozdil@outlook.com>
2022-11-21 10:43:57 -0500

    retreive entities with their relations

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -13,11 +13,20 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({
+      relations: {
+        flavors: true,
+      },
+    });
   }
 
   async findOne(id: string) {
-    const coffee = await this.coffeeRepository.findOne({ where: { id: +id } });
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id: +id },
+      relations: {
+        flavors: true,
+      },
+    });
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
     }
