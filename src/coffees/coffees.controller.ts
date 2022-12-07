cff44c624a27a1a2205d63df0aeb51933618cf7b
Yamac Ozdil <yamac.ozdil@outlook.com>
2022-12-07 17:54:48 +0300

    Timeout interceptor

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -29,7 +29,8 @@ export class CoffeesController {
   // @SetMetadata('isPublic', true)
   @Public()
   @Get()
-  findAll(@Query() paginationQuery: PaginationQueryDto) {
+  async findAll(@Query() paginationQuery: PaginationQueryDto) {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
     return this.coffeesService.findAll(paginationQuery);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 // import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
 
@@ -17,8 +18,11 @@ async function bootstrap() {
       },
     }),
   );
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalInterceptors(new WrapResponseInterceptor());
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
+  );
   // app.useGlobalGuards(new ApiKeyGuard());
   await app.listen(3000);
 }
